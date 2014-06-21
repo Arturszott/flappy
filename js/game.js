04b46e6612a55f49e767ebb2ee762e17f79ad718
Artur Szott <a.bartimeus@gmail.com>
2014-06-21 14:39:25 +0200

    game.js

--- js/game.js
+++ js/game.js
@@ -3,17 +3,21 @@
 
 //global variables
 window.onload = function () {
-  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'flappy-timberman');
-
-  // Game States
-  game.state.add('boot', require('./states/boot'));
-  game.state.add('menu', require('./states/menu'));
-  game.state.add('play', require('./states/play'));
-  game.state.add('preload', require('./states/preload'));
-  
-
-  game.state.start('boot');
+	document.addEventListener("deviceready",onDeviceReady,false);
 };
+window.onDeviceReady = function () {
+	console.log(Media);
+	var game = new Phaser.Game(288, 505, Phaser.AUTO, 'flappy-hell');
+
+	// Game States
+	game.state.add('boot', require('./states/boot'));
+	game.state.add('menu', require('./states/menu'));
+	game.state.add('play', require('./states/play'));
+	game.state.add('preload', require('./states/preload'));
+	
+
+	game.state.start('boot');
+}
 },{"./states/boot":7,"./states/menu":8,"./states/play":9,"./states/preload":10}],2:[function(require,module,exports){
 'use strict';
 
